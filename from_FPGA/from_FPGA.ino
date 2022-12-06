af5807a82abb3fd90fc7c49f3b003e044c8ca977
Lele Zhou <46782586+zhoulele12@users.noreply.github.com>
2022-12-05 22:37:28 -0500

    Add files via upload

--- from_FPGA/from_FPGA.ino
+++ from_FPGA/from_FPGA.ino
@@ -12,6 +12,8 @@ struct Signal{
 };
 
 Signal data;
+#define BRAKE_PEDAL A0 // the FSR and 10K pulldown are connected to A0
+#define GAS_PEDAL A1
 #define acc0 A2
 #define acc1 A3
 #define acc2 A4
@@ -24,6 +26,9 @@ void ResetData()
 }
 void setup() {
   Serial.begin(9600);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
   radio.begin();
   radio.openWritingPipe(pipeOut);
   radio.stopListening();
@@ -32,6 +37,33 @@ void setup() {
  
 void loop() {
   // put your main code here, to run repeatedly:
+  int brakeReading = analogRead(BRAKE_PEDAL);
+  int gasReading = analogRead(GAS_PEDAL);
+  
+  if (brakeReading > 10){
+      digitalWrite(5, HIGH);
+      data.throttle = 0;
+  }
+  else{
+    digitalWrite(5, LOW);
+
+    if ((gasReading > 10 && gasReading <= 350))
+      {
+        digitalWrite(3, HIGH);
+        digitalWrite(4, LOW);
+
+      } else if (gasReading >350 && gasReading <=650) {
+        digitalWrite(3, LOW);
+        digitalWrite(4, HIGH);
+      } else if (gasReading > 650) {
+        digitalWrite(4, HIGH);
+        digitalWrite(3, HIGH);
+      } else{
+        digitalWrite(4, LOW);
+        digitalWrite(3, LOW);
+      }
+    data.throttle = map(gasReading,0,1023,0,255);
+  }
   int bit1 = analogRead(acc0);
   int bit2 = analogRead(acc1);
   int bit3 = analogRead(acc2);
@@ -49,35 +81,15 @@ void loop() {
   if (bit4 > 500){
     turn_val = turn_val + 8;
   }
-  // Serial.println(turn_val);
-//  Serial.println("bit1");
-//  Serial.println(bit1);
-//  Serial.println("bit2");
-//  Serial.println(bit2);
-//  Serial.println("bit3");
-//  Serial.println(bit3);
-//  Serial.println("bit4");
-//  Serial.println(bit4);
+
   // 9 to 0 -> left
   // 15 to 10 -> right
-//  if(turn_val<8){
-//    data.throttle = (8-turn_val)*10;
-//  } else {
-//    data.throttle = 80 + (16-turn_val)*20;
-//  }
+
   if(turn_val<8){
-    data.throttle = (8-turn_val)*10;
+    data.turn = (8-turn_val)*16;
   } else {
-    data.throttle = 80 + (16-turn_val)*20;
+    data.turn = 128 + (16-turn_val)*16;
   }
-  
-//  int bxValue = analogRead(joyBX);
-//  int byValue = analogRead(joyBY);
-
-//  data.throttle = map(bxValue,0,1023,0,255);
-//  data.turn = map(byValue,0,1023,0,255);
-
-  data.turn = 128;
   Serial.println(data.throttle);
   radio.write(&data,sizeof(Signal));
   delay(100);
