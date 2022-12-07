d2b941496787ff15394da0728472ed6bb4f33122
Lele Zhou <46782586+zhoulele12@users.noreply.github.com>
2022-12-07 11:25:22 -0500

    Add files via upload

--- mult.s
+++ mult.s
@@ -2,30 +2,8 @@
 nop
 nop
 nop
-addi	$r1, $r1, 2
-addi 	$r2, $r2, 5
-add	$r2, $r2, $r1
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-mul     $r28, $r1, $r2
-nop
+addi	$r1, $r1, 5
+addi 	$r2, $r2, 2
 nop
 nop
 nop
@@ -45,6 +23,26 @@ nop
 nop
 nop
 start:
+	mul    $r28, $r1, $r2
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
 	nop
 	nop
 	j start
+
